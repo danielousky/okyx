937a4dee071c379f65abdd107c5bcd6704621f20
Daniel Moros <danielousky@gmail.com>
2024-09-23 20:07:10 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create(email: 'moros.daniel@gmail.com', first_name: 'Daniel', last_name: 'Moros', number_phone: '5555555', password: '123123')
+User.create(email: 'juliomoros@gmail.com', first_name: 'Julio', last_name: 'Moros', number_phone: '5555555', password: '123123')
+User.create(email: 'elenaquinteroc@gmail.com', first_name: 'Julio', last_name: 'Moros', number_phone: '5555555', password: '123123')
