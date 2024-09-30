775bee6af8e49687adea45718c3aa86ace266335
Daniel Moros <danielousky@gmail.com>
2024-09-30 18:29:49 -0400

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,7 +5,39 @@ import "popper"
 import "bootstrap"
 import "trix"
 import "@rails/actiontext"
-import "./custom.js";
+
+
+import jquery from 'jquery';
+window.jQuery = jquery;
+window.$ = jquery;
+document.addEventListener('DOMContentLoaded', function () {
+
+	$('.onlyOneCharacter').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val());
+	});
+
+	$('.upcase').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val().toUpperCase());
+	});
+
+	$('.onlyNumbers').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val().replace(/[^0-9]/g, ''));
+	});
+
+	$('.maxSize').on('input', function(e) {
+		let node = $(this);
+
+		let size = node.attr('size')
+		if (size == null) { size = 11 };
+		size = parseInt(size)
+		node.val(node.val().substring(0, size))
+	})
+});
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
