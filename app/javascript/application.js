33a384d12cb76f72f6797d0a6bcc069a75f955ec
Daniel Moros <danielousky@gmail.com>
2024-10-01 06:55:12 -0400

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,41 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "/javascript/custom"
 import "popper"
 import "bootstrap"
 import "trix"
 import "@rails/actiontext"
+
+
+import jquery from 'jquery';
+window.jQuery = jquery;
+window.$ = jquery;
+document.addEventListener('DOMContentLoaded', function () {
+
+	$('.onlyOneCharacter').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val());
+	});
+
+	$('.upcase').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val().toUpperCase());
+	});
+
+	$('.onlyNumbers').on('input', function(evt) {
+		var node = $(this);
+		node.val(node.val().replace(/[^0-9]/g, ''));
+	});
+
+	$('.maxSize').on('input', function(e) {
+		let node = $(this);
+
+		let size = node.attr('size')
+		if (size == null) { size = 11 };
+		size = parseInt(size)
+		node.val(node.val().substring(0, size))
+	})
+});
 
 
 
