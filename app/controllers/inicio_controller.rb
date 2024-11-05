0ae112072ab6ce814fc1128347a9b42285160a67
Daniel Moros <danielousky@gmail.com>
2024-11-05 16:25:03 -0400

    Update search form and several changes

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -2,10 +2,11 @@ class InicioController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   layout 'outside'
   def index
-    if params[:search] or params[:area_id]
+    if params[:search] or params[:area_id] or params[:location]
 
       @services = params[:area_id] ? Area.where(id: params[:area_id].to_s).first&.services : Service.all
 
+      @services = @services.by_state params[:location] if params[:location]
       @services = @services.search_by params[:search] if params[:search]
 
     else
