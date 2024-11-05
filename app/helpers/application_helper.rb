a61fcce15f486da54feeb839996eca08aa05b77a
Daniel Moros <danielousky@gmail.com>
2024-11-05 16:22:13 -0400

    Delete round-0 class in butons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def btn_tooltip_link_to(title_tooltip, title, icon_name, btn_type, path)
-		link_to path, 'data-bs-toggle': 'tooltip', title: title_tooltip, class: "btn rounded-0 #{btn_type}" do
+		link_to path, 'data-bs-toggle': 'tooltip', title: title_tooltip, class: "btn #{btn_type}" do
 			capture_haml { "<i class='#{icon_name}'></i> #{title}".html_safe }
 		end
 	end
