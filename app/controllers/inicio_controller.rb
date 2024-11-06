cf8ad28038a3a17af27dccd5718636f96862a245
Daniel Moros <danielousky@gmail.com>
2024-11-06 13:27:38 -0400

    Updated view on services

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -4,11 +4,22 @@ class InicioController < ApplicationController
   def index
     if params[:search] or params[:area_id] or params[:location]
 
-      @services = params[:area_id] ? Area.where(id: params[:area_id].to_s).first&.services : Service.all
+      @services = params[:area_id].blank? ? Service.all : Area.where(id: params[:area_id].to_s).first&.services
 
       @services = @services.by_state params[:location] if params[:location]
       @services = @services.search_by params[:search] if params[:search]
 
+      if @services.any?
+        if @services.count > 20
+          flash[:success] = " ¡Más de 20 resultados encontrados! Con el filtro, puedes ser más específico para encontrar el servicio que necesitas!"
+        else
+          flash[:success] = "#{@services.count} #{'resultado'.pluralize(@services.count)} #{'encontrado'.pluralize(@services.count)}!"
+        end
+      else
+        flash[:danger] = "#{@services.count} #{'resultado'.pluralize(@services.count)} #{'encontrado'.pluralize(@services.count)}. Por favor, intente buscando con otra palabra clave"
+      end
+      @services = @services.limit(10)
+      
     else
       reset_session
     end
